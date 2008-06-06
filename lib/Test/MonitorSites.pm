499c1a6708d0dbf21fd1a7259c3d7515d43978cf
Hugh Esco <hesco@campaignfoundations.com>
2008-06-06 20:06:10 +0000

    Import of HESCO/Test-MonitorSites-0.14 from CPAN.
    gitpan-cpan-distribution: Test-MonitorSites
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         HESCO/Test-MonitorSites-0.14.tar.gz
    gitpan-cpan-author:       HESCO
    gitpan-cpan-maturity:     released

--- lib/Test/MonitorSites.pm
+++ lib/Test/MonitorSites.pm
@@ -16,7 +16,7 @@ use Mail::Mailer;
 # use Mail::Mailer qw(mail);
 
 use vars qw($VERSION);
-$VERSION = '0.13';
+$VERSION = '0.14';
 
 1; # Magic true value required at end of module
 
@@ -492,7 +492,7 @@ Test::MonitorSites - Monitor availability and function of a list of websites
 
 =head1 VERSION
 
-This document describes Test::MonitorSites version 0.13
+This document describes Test::MonitorSites version 0.14
 
 =head1 SYNOPSIS
 
